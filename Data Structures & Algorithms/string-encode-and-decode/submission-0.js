d45321df2ef0c9d223a4890b4f145380d7d07616
Thiyagu Arunachalam <101319072+Thiya11@users.noreply.github.com>
2026-07-19 12:05:57 +0900

    Add: string-encode-and-decode - submission-0

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-0.js
@@ -0,0 +1,30 @@
+class Solution {
+    /**
+     * @param {string[]} strs
+     * @returns {string}
+     */
+    encode(strs) {
+        let result = ""
+        for (const str of strs) {
+            result += str.length + "#" + str
+        }
+        return result
+    }
+
+    /**
+     * @param {string} str
+     * @returns {string[]}
+     */
+    decode(str) {
+       const result = []
+       let i = 0
+       while (i < str.length) {
+        let j = i
+        while (str[j] !== "#") j++
+        const len = parseInt(str.slice(i, j))
+        result.push(str.slice(j + 1, j + 1 + len))
+        i = j + 1 + len
+       }
+       return result
+    }
+}
